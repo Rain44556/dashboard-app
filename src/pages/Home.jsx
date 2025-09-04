858f0d86057d767d576ed0ef5008e25ff4d8132b
rain44556 <bristy44556@gmail.com>
2025-09-04 23:40:07 +0600

    Add animation in user management title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import UserList from './UserList';
+import { motion } from 'framer-motion';
 
 const Home = () => {
     return (
         <div>
-        <h1 className='font-bold text-2xl mb-3 text-gray-800'>User Management</h1>
+    <motion.h1
+      className="font-bold text-2xl mb-3 text-gray-800"
+      initial={{ opacity: 0, y: -20 }} 
+      animate={{ opacity: 1, y: 0 }}   
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
+      User Management
+    </motion.h1>
         <UserList></UserList>
         </div>
     );
